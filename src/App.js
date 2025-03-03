a43eee5b178065efafbe54a0ef800348ba21f368
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-03 22:37:36 +0100

    add new todo item onClick to todos array

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ function App() {
     { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User Three" },
   ];
 
+  // const addTodo = () => alert("Add new todo");
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  };
+
   return (
     <div className="container mt-5">
       <div className="card text-white bg-dark mb-3">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <TodoTable 
-            todos={todos}
-          />
+          <TodoTable todos={todos} />
+          <button className="btn btn-primary mx-5 my-1" onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
