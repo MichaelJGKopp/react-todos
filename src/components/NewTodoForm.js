4f157889fc60d6f96d6d71f3e57fdbb4a6cf7b30
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-04 09:41:44 +0100

    listen to onChange events in form

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,16 +1,33 @@
 import React from "react";
 
 function NewTodoForm() {
+  const descriptionChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const assignedChange = (event) => {
+    console.log("assigned", event.target.value)
+  }
+
   return (
     <div className="mt-5">
       <form>
         <div className="mb-3">
           <label className="form-label">Assigned</label>
-          <input type="text" className="form-control" required></input>
+          <input type="text" 
+          className="form-control" 
+          required
+          onChange={assignedChange}
+          ></input>
         </div>
         <div className="mb-3">
           <label className="form-label">Description</label>
-          <textarea className="form-control rows={3}" required></textarea>
+          <textarea 
+          className="form-control" 
+          rows={3} 
+          required
+          onChange={descriptionChange}
+          ></textarea>
         </div>
         <button type="button" className="btn btn-primary mt-3">
           Add Todo
