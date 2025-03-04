891bc5a942d1079d231a244614592e719c545400
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-04 11:23:14 +0100

    add delete button

--- src/App.js
+++ src/App.js
@@ -15,14 +15,32 @@ function App() {
 
   // function to add a new todo onClick
   const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+
     if (todos.length > 0) {
-      const newTodo = {
-        rowNumber: todos.length + 1,
-        rowDescription: description,
-        rowAssigned: assigned,
-      };
-      setTodos([...todos, newTodo]);
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
     }
+
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (rowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== rowNumber;
+    });
+
+    filtered.map((item) => {
+      item.rowNumber = filtered.indexOf(item) + 1;
+      return item;
+    });
+    setTodos([...filtered]);
   };
 
   return (
@@ -30,7 +48,7 @@ function App() {
       <div className="card text-white bg-dark mb-3">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <TodoTable todos={todos} />
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
           <button className="btn btn-primary mx-5 my-1" onClick={addTodo}>
             Add new todo
           </button>
--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,12 +1,19 @@
 function TodoRowItem(props) {
-  
-    return (
+  return (
     // can only return one element, wrap in div if necessary
-      <tr>
-        <th scope="row">{props.rowNumber}</th>
-        <td>{props.rowDescription}</td>
-        <td>{props.rowAssigned}</td>
-      </tr>
+    <tr>
+      <th scope="row">{props.rowNumber}</th>
+      <td>{props.rowDescription}</td>
+      <td>{props.rowAssigned}</td>
+      <td>
+        <button
+          className="btn btn-danger"
+          onClick={() => props.deleteTodo(props.rowNumber)}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
   );
 }
 
